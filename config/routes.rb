884659786bd37e87b88c720d712a2fbb66c779bf
Michael <mich45@gmail.com>
2011-11-16 18:44:48 +0200

    Done with the sin in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 SampleApp::Application.routes.draw do
-  resources :users
 
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
   match '/signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
